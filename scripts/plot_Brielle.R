32b70beeebf29cbde34d75479bafee3fdbd4f731
briellekwarta19e <brielle.kwarta@gmail.com>
2021-10-21 10:37:22 -0700

    Brielle's bad plot

---
+++ scripts/plot_Brielle.R
@@ -0,0 +1,7 @@
+library(here)
+library(tidyverse)
+file_path <- here::here("data", "pokemon.csv")
+poke_dat <- read.csv(file.path, header = T)
+
+#ggplot
+ggplot(poke_dat) + geom_point(mapping = aes(english_name, percent_female))
