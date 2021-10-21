941823712c1078a08892505988008403368fe41a
Zoe Rand <zrand@uw.edu>
2021-10-21 10:41:52 -0700

    edited Brielle and my plots

--- scripts/plot_Zoe.R
+++ scripts/plot_Zoe.R
@@ -2,4 +2,4 @@ library(tidyverse)
 pokemon<-read_csv("data/pokemon.csv")
 
 
-ggplot(pokemon) + geom_histogram(aes(gen), stat = "count")
+ggplot(pokemon) + geom_histogram(aes(gen), stat = "count", color = "green")
