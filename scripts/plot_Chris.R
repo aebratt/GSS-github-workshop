7bc0d51f3fe49c92255e4ed2bb5595a3eb828956
ChrisMantegna <christine.mantegna@gmail.com>
2021-11-04 12:49:55 -0700

    wrong

--- scripts/plot_Chris.R
+++ scripts/plot_Chris.R
@@ -1,7 +1,7 @@
 getwd()
 setwd(~data)
 #note that setwd sometimes needs the whole path, not just the end
-
+5 + 5
 pk<- read.csv("C:/Users/chris/Dropbox/PC/Documents/GitHub/ChrisMantegna/GSS-github-workshop/data/pokemon.csv")
 head(pk)
 plot(x= pk$english_name , y= pk$capture_rate,
