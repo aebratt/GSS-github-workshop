7a2ee9754c2e06840382df00a38ca9a395da9d81
Zoe Rand <zrand@uw.edu>
2021-11-04 12:49:34 -0700

    edited chris plot

--- scripts/plot_Chris.R
+++ scripts/plot_Chris.R
@@ -1,7 +1,7 @@
 getwd()
 setwd(~data)
 #note that setwd sometimes needs the whole path, not just the end
-
+print("hello world")
 pk<- read.csv("C:/Users/chris/Dropbox/PC/Documents/GitHub/ChrisMantegna/GSS-github-workshop/data/pokemon.csv")
 head(pk)
 plot(x= pk$english_name , y= pk$capture_rate,
